1655d5e7a1860278a5e330c5bb09c61efaceba6d
Maharram Guliyev <56450053+maharramg@users.noreply.github.com>
2026-03-02 16:58:33 +0200

    cleanup

--- lib/app.dart
+++ lib/app.dart
@@ -10,19 +10,6 @@ import 'package:portfolio/utilities/strings.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
-  static const _knownRoutes = [
-    Routes.homeScreen,
-    Routes.aboutScreen,
-    Routes.projectsScreen,
-  ];
-
-  static String get _initialRoute {
-    final path = Uri.base.path;
-    if (path.isEmpty || path == '/') return Routes.homeScreen;
-    if (_knownRoutes.contains(path)) return path;
-    return Routes.homeScreen;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,13 +20,12 @@ class App extends StatelessWidget {
         scaffoldBackgroundColor: scaffoldBgColor,
         colorSchemeSeed: primaryColor,
       ),
-      initialRoute: _initialRoute,
+      initialRoute: Routes.homeScreen,
       routes: {
         Routes.homeScreen: (context) => const HomeScreen(),
         Routes.aboutScreen: (context) => const AboutScreen(),
         Routes.projectsScreen: (context) => const AllProjectsScreen(),
       },
-      onUnknownRoute: (settings) => MaterialPageRoute(builder: (_) => const HomeScreen()),
     ).animate().fadeIn(duration: 400.ms);
   }
 }
